62966f87b1ace7351f0570112b005cb1d1a0a779
oceancx <oceancx@gmail.com>
2016-06-19 21:58:30 +0800

    更新音乐

--- public/js/function6.js
+++ public/js/function6.js
@@ -1,14 +1,8 @@
-$(function(){
-	$("#song").click(function() {
-		var scrwidth = (screen.width-50 < 430?screen.width-50:430);
-		var host='http://music.yayayouji.com';
-		var pattern= /[0-9]+/;
-		var link=$("#song_id").html().match(pattern)[0];
-        $("#listen").html('<center><iframe src="'+host+'/xiamiplayer/'+link+'/" frameborder="0" scrolling="0" width="'+scrwidth+'" height="200" allowtransparency></iframe></center>');
-        $("#listenpanel").modal();
-	});
-});
-
-function listen() {
-	alert("alkqwe");
-}
+function listen(content) {
+       var scrwidth = (screen.width-50 < 430?screen.width-50:430);
+       var host='http://music.yayayouji.com';
+       var pattern=/[0-9]+/;
+       var link=content.match(pattern)[0];
+       $("#listen").html('<center><iframe src="'+host+'/xiamiplayer/'+link+'/" frameborder="0" scrolling="0" width="'+scrwidth+'" height="200" allowtransparency></iframe></center>');
+       $("#listenpanel").modal();
+ }
